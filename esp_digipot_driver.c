c73d76698823eb39f8980b24ec3a019f8435f28f
nichonaugle <naugle.nicho@gmail.com>
2025-03-04 21:38:39 -0600

    Added deinitialization. Fixed some minor errors

--- esp_digipot_driver.c
+++ esp_digipot_driver.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include "esp_digipot_driver.h"
+#include "digipot_configs.h"
 #include "esp_err.h"
+#include "esp_log.h"
 #include "driver/i2c_master.h"
 
-static const char* TAG = "digipot-driver"
+static const char* TAG = "digipot-driver";
 
-esp_err_t digipot_init(digipot_handle_t *digipot_handle, digipot_type_t digipot_type, gpio_num_t sda_gpio, gpio_num_t scl_gpio, size_t clock_speed) {
+esp_err_t digipot_init(digipot_handle_t *digipot_handle, i2c_master_bus_handle_t bus_handle, digipot_type_t digipot_type, gpio_num_t sda_gpio, gpio_num_t scl_gpio, size_t clock_speed) {
     if (sda_gpio == NULL || scl_gpio == NULL || clock_speed == NULL || digipot_handle == NULL) {
         ESP_LOGE(TAG, "Digipot improperly initialized!");
         return ESP_ERR_INVALID_ARG;
@@ -16,18 +18,18 @@ esp_err_t digipot_init(digipot_handle_t *digipot_handle, digipot_type_t digipot_
         return ESP_ERR_INVALID_ARG;
     }
 
-    digipot_handle = digipot_config_table[digipot_type]
+    digipot_handle = digipot_config_table[digipot_type];
 
-    digipot_handle.dev_cfg = {
-            .address = digipot_handle.i2c_address,
-            .clk_speed = clock_speed,
-            .sda_io_num = sda_gpio,
-            .scl_io_num = scl_gpio
-        },
+    digipot_handle->dev_cfg = {
+        .address = digipot_handle.i2c_address,
+        .clk_speed = clock_speed,
+        .sda_io_num = sda_gpio,
+        .scl_io_num = scl_gpio
+    };
 
-    esp_err_t err = i2c_master_bus_add_device(bus_handle, &digipot_handle.dev_cfg, dev_handle);
+    esp_err_t err = i2c_master_bus_add_device(bus_handle, digipot_handle->dev_cfg, digipot_handle->dev_handle);
     if (err != ESP_OK) {
-        ESP_LOGE(TAG, "Failed to add digipot to bus!")
+        ESP_LOGE(TAG, "Failed to add digipot to bus!");
         return err;
     }
     ESP_LOGI(TAG, "Added digipot to bus. Initializing...");
@@ -48,20 +50,20 @@ esp_err_t digipot_set_wiper_position(uint8_t val, digipot_handle_t *digipot_hand
     return digipot_register_write(data, 2, digipot_handle);
 }
 
-esp_err_t digipot_register_write(uint8_t *tx_buf, uint8_t tx_buf_size, digipot_handle_t *digipot_handle) {
-    if (digipot_handle == NULL || tx_buf == NULL) {
-        ESP_LOGE(TAG, "Invalid arguments for register write!");
-        return ESP_ERR_INVALID_ARG;
-    }
-    return i2c_master_transmit(digipot_handle->i2c_port, digipot_handle->i2c_addr, tx_buf, tx_buf_size, digipot_handle.transaction_timeout_ms / portTICK_PERIOD_MS);
-}
-
 esp_err_t digipot_register_read(uint8_t reg_to_read, uint8_t *rx_buf, uint8_t rx_buf_size, digipot_handle_t *digipot_handle) {
     if (digipot_handle == NULL || rx_buf == NULL) {
         ESP_LOGE(TAG, "Invalid arguments for register read!");
         return ESP_ERR_INVALID_ARG;
     }
-    return i2c_master_transmit_receive(digipot_handle->i2c_port, &reg_to_read, digipot_handle->i2c_addr, rx_buf, rx_buf_size, digipot_handle.transaction_timeout_ms / portTICK_PERIOD_MS);
+    return i2c_master_transmit_receive(digipot_handle->dev_handle, &reg_to_read, sizeof(reg_to_read), rx_buf, rx_buf_size, digipot_handle->transaction_timeout_ms);
+}
+
+esp_err_t digipot_register_write(uint8_t *tx_buf, uint8_t tx_buf_size, digipot_handle_t *digipot_handle) {
+    if (digipot_handle == NULL || tx_buf == NULL) {
+        ESP_LOGE(TAG, "Invalid arguments for register write!");
+        return ESP_ERR_INVALID_ARG;
+    }
+    return i2c_master_transmit(digipot_handle->dev_handle, tx_buf, tx_buf_size, digipot_handle->transaction_timeout_ms);
 }
 
 esp_err_t digipot_soft_reset(digipot_handle_t *digipot_handle) {
@@ -71,4 +73,8 @@ esp_err_t digipot_soft_reset(digipot_handle_t *digipot_handle) {
     }
     uint8_t soft_reset_cmd = digipot_handle->soft_reset_cmd;
     return digipot_register_write(&soft_reset_cmd, 1, digipot_handle);
+}
+
+esp_err_t digipot_deinit(digipot_handle_t *digipot_handle) {
+    return i2c_master_bus_rm_device(digipot_handle->dev_handle);
 }
--- include/esp_digipot_driver.h
+++ include/esp_digipot_driver.h
@@ -5,10 +5,11 @@ typedef struct {
     char device_name[16];  // Max of 16 characters, including null terminator
     i2c_port_t i2c_port;  // I2C port number
     i2c_device_config_t dev_cfg;
+    i2c_master_dev_handle_t dev_handle;
     uint8_t i2c_address;  // I2C address of the digipot
     uint8_t wiper_reg;
     uint8_t max_position; // Max position of the digipot
-    uint32_t min_position; // Min position of the digipot
+    uint8_t min_position; // Min position of the digipot
     uint8_t wiper_bit_count; // Bit resolution of the wiper, typically 7 or 8 bits
     uint8_t soft_reset_cmd;
     uint32_t transaction_timeout_ms;
@@ -18,14 +19,16 @@ typedef enum {
     AD5246,
     /* Add more variants here if needed */
     DIGIPOT_COUNT
-} digipot_type_t
+} digipot_type_t;
 
-esp_err_t digipot_init(digipot_handle_t *digipot_handle);
+esp_err_t digipot_init(digipot_handle_t *digipot_handle, digipot_type_t digipot_type, gpio_num_t sda_gpio, gpio_num_t scl_gpio, size_t clock_speed);
 
 esp_err_t digipot_set_wiper_position(uint8_t val, digipot_handle_t *digipot_handle);
 
-esp_err_t digipot_register_read(uint8_t *rx_buf, uint8_t rx_buf_size, digipot_handle_t *digipot_handle);
+esp_err_t digipot_register_read(uint8_t reg_to_read, uint8_t *rx_buf, uint8_t rx_buf_size, digipot_handle_t *digipot_handle);
 
 esp_err_t digipot_register_write(uint8_t *tx_buf, uint8_t tx_buf_size, digipot_handle_t *digipot_handle);
 
 esp_err_t digipot_soft_reset(digipot_handle_t *digipot_handle);
+
+esp_err_t digipot_deinit(digipot_handle_t *digipot_handle);
